043367cb6306e166a2a11c1defa3e5309d7acbf9
SHAIK FOUZIA BANU <136546551+fouziaban@users.noreply.github.com>
2024-07-09 15:09:34 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ variable "private_key_path" {
 
 # Security Group
 resource "aws_security_group" "strapi_sg" {
-  name        = "banu-strapi2-security-group"
+  name        = "banu-strapi-2-security-group"
   description = "Security group for Strapi EC2 instance"
 
   ingress {
